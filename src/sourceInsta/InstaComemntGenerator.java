d89f14cc66098c2530667f5b994bf60c8ee301ba
pedrohbcavalcante <pedrinho.sjs@ufrn.edu.br>
2016-09-25 01:48:15 -0300

    Começando a adaptar para dois

--- src/sourceInsta/InstaComemntGenerator.java
+++ src/sourceInsta/InstaComemntGenerator.java
@@ -20,23 +20,27 @@ public class InstaComemntGenerator {
                 String[] usernames = {"dearyagoo", "cleonicekelly_", "hannafariias", "ivethearaujo", 
                 		"yasmimluh", "daysemrss", "juliamariaazevedo", "juliamdrs_", "shelucena", "tatjanamirela", 
                 		"srt_brito", "_helomdrs", "rillary.alves", "othondelfino", "ofelipemotta"};
-
-                // PARA DOIS NICKS, COMENTAR A LINHA ABAIXO
-                //for (int i = 0; i < usernames.length - 2; i++) {
-                	// PARA DOIS NICKS, COMENTAR A LINHA ABAIXO
-                  //  for (int j = i + 1; j < usernames.length - 1; j++) {
-                    	// PARA DOIS LINKS, DESCOMENTAR A LINHA ABAIXO
-                	for (int j = 0; j < usernames.length - 1; j++) { 
+                // Para três nicks, utilizar este script e comentar o de dois nicks
+                
+                for (int i = 0; i < usernames.length - 2; i++) {
+                    for (int j = i + 1; j < usernames.length - 1; j++) {
                         for (int k = j + 1; k < usernames.length; k++) {
-                        	
-                    //        pw.println("@" + usernames[i] + " @" + usernames[j] + " @" + usernames[k]);
-                        	//PARA DOIS LINKS DESCOMENTAR A LINHA ABAIXO
-                        	pw.println(" @" + usernames[j] + " @" + usernames[k]);
-
+                            pw.println("@" + usernames[i] + " @" + usernames[j] + " @" + usernames[k]);
                             count++;
                         }
                     }
-                //}
+                }
+                
+                
+                /* Para dois nicks, utilizar esses parâmetos aqui
+                 
+				for (int j = 0; j < usernames.length - 1; j++) {
+                	for (int k = j + 1; k < usernames.length; k++) {
+						pw.println(" @" + usernames[j] + " @" + usernames[k]);
+                   	}
+				}
+                 
+                 */
 
                 pw.println();
                 pw.println("Iterations: " + count);
